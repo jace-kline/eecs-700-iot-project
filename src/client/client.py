eef0bf6fd04cb442b2fbf09250841ca457eb25c9
Jace Kline <jace.a.kline@gmail.com>
2022-03-23 22:17:19 -0500

    Initial commit. Created basic client script

---
+++ src/client/client.py
@@ -0,0 +1,61 @@
+from .lib import create_mqtt_client, Device, Timer
+import sys
+import time
+import json
+
+# when publishing, start a timer
+def on_publish(client, userdata, mid):
+    client.timer.start()
+
+# when receiving, stop timer and record duration
+def on_message(client, userdata, message):
+    dur = client.timer.stop()
+    print(dur)
+
+def main():
+    # read in client name as argument
+    config_yml_path = sys.argv[1] if len(sys.argv) > 1 else None
+
+    if config_yml_path is None:
+        print("Supply MQTT client config YAML file as argument to this script")
+        exit(1)
+
+    # create Paho MQTT client from YAML config file
+    client = create_mqtt_client(config_yml_path)
+
+    # add a timer object to the client so it can be started/stopped when publishing/receiving
+    client.timer = Timer()
+
+    # set callback function for receiving messages
+    client.on_message = on_message
+
+    # start new thread to listen to incoming events
+    client.loop_start()
+
+    # instantiate device that produces data
+    device = Device()
+
+    iterations = 24 # number of loops
+    interval = 5 # seconds
+
+    # publish new data in a loop
+    for _ in range(0, iterations):
+        # read device data
+        value = device.read()
+
+        # publish the data
+        topic = f'device/{client.name}/data'
+        payload = json.dumps({ 'value': value })
+        client.publish(topic, payload)
+
+        # wait before looping again
+        time.sleep(interval)
+
+    # save the results of the publish->receive durations to a file
+    # durs = client.timer.durations()
+    # for dur in durs:
+    #     print(dur)
+
+# if this script is executed directly (not imported), then run main()
+if __name__ == "__main__":
+    main()
---
+++ src/client/lib.py
@@ -0,0 +1,92 @@
+import yaml
+import ssl
+import random
+from timeit import default_timer as timer
+import paho.mqtt.client as paho_client
+
+def load_yaml(path):
+    with open(path, 'r') as stream:
+        return yaml.load(stream, yaml.Loader)
+
+def create_mqtt_client(config_yml_path):
+
+    def on_connect(client, obj, flags, rc):
+        if rc == 0:
+            print("MQTT connection successful")
+        else:
+            print("MQTT connection failed")
+            client.disconnect()
+
+    # load config YAML file
+    config = load_yaml(config_yml_path)
+
+    # set client params
+    client_name = config['client_name']
+    client = paho_client.Client(client_name)
+    client.name = client_name
+
+    # if TLS enabled, set TLS properties
+    if bool(config['tls_enabled']):
+        client.tls_set(
+            ca_certs=config['cafile'],
+            certfile=config['certfile'],
+            keyfile=config['keyfile'],
+            tls_version=ssl.PROTOCOL_SSLv23
+        )
+        client.tls_insecure_set(True)
+    
+    client.on_connect = on_connect
+
+    # attempt to connect to broker
+    client.connect(config['broker_ip'], config['port'], 30)
+
+    # subscribe to topics in 'subscriptions' list from config file
+    if "subscriptions" in config.keys():
+        for topic in config['subscriptions']:
+            client.subscribe(topic)
+            print(f"Subscribed to {topic}")
+
+    return client
+
+# A mock device that returns random numbers as the "data"
+class Device:
+    def __init__(self):
+        random.seed(1)
+
+    def read(self):
+        return random.randint()
+
+class Timer:
+    def __init__(self):
+        self.begin = None
+        self.end = None
+        self.times = []
+
+    def reset(self):
+        self.begin = None
+        self.end = None
+
+    def start(self):
+        self.begin = timer()
+
+    def stop(self):
+        self.end = timer()
+        if self.begin is None:
+            self.reset()
+            raise RuntimeError("Timer never started with start() method")
+        else:
+            dur = self.end - self.begin
+            self.times.append(dur)
+            self.reset()
+            return dur
+
+    def last(self):
+        if len(self.times) == 0:
+            raise RuntimeError("No recorded duration values")
+        else:
+            return self.times[-1]
+
+    def durations(self):
+        return self.times
+        
+        
